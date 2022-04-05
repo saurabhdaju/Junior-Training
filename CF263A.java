b26fc0c3e8623d26d1a9b14675a599ded4b2e508
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-04-05 13:20:36 +0530

    OG approach

---
+++ CF263A.java
@@ -0,0 +1,10 @@
+package JT;
+import java.util.Scanner;
+public class CF263A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<25; i++)  {
+            System.out.println(sc.nextInt()==1 ? Math.abs(i/5-2) + Math.abs(i%5-2) : "");
+        }
+    }
+}
