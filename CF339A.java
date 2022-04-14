ec5795a07a74cdf3079e3d2d7366c4d713b70ae2
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-04-14 20:43:22 +0530

    Helpful Maths

---
+++ CF339A.java
@@ -0,0 +1,11 @@
+package JT;
+import java.util.Arrays;
+import java.util.Scanner;
+public class CF339A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.next().split("\\+");
+        Arrays.sort(arr);
+        System.out.println(String.join("+", arr));
+    }
+}
