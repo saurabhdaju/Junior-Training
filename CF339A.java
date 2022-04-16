d3d5e1bf08d32ecd0012ccc9423d72d0d4f3eacb
Saurabh Pandey <saurabhpandey8707@gmail.com>
2022-04-16 11:39:44 +0530

    Helpful Maths

--- CF339A.java
+++ CF339A.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class CF339A {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);     //OG APPROACH
         String[] arr = sc.next().split("\\+");
         Arrays.sort(arr);
         System.out.println(String.join("+", arr));
